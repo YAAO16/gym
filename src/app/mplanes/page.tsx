bc8df5dd179cde6695a508c22ffb270c9623e588
YAAO16 <caetero3142687740@gmail.com>
2024-05-07 21:52:38 -0500

    frontend-beta

--- src/app/mplanes/page.tsx
+++ src/app/mplanes/page.tsx
@@ -1,65 +1,56 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import { Close as CloseIcon, Edit as EditIcon, Delete as DeleteIcon } from '@mui/icons-material';
+import { Close as CloseIcon } from '@mui/icons-material';
 
-export default function mplanes() {
-   const data = [
-    { id: 1, name: 'Harold Mosquera', tipo: 'estudiante' },
-    { id: 2, name: 'Esteban Josa', tipo: 'privado' },
-    { id: 3, name: 'Maicol Guerrero', tipo: 'mensual' },
-  ];
-
+export default function Planes() {
   const tableWidth = '60%'; 
   const tableHeight = 'auto'; 
+
+  const gymPlans = [
+    { name: 'Plan Básico', price: 29.99, features: ['Acceso al gimnasio', 'Clases grupales limitadas', 'Sin entrenador personal'], image: 'https://www.bailonga.com/pics/2023/02/como-crear-un-plan-de-entrenamiento-para-realizar-en-casa.png' },
+    { name: 'Plan Estándar', price: 49.99, features: ['Acceso al gimnasio', 'Clases grupales ilimitadas', '1 sesión de entrenador personal por semana'], image: 'https://www.cambiatufisico.com/wp-content/uploads/rutina-gimnasio-anual3.jpg' },
+    { name: 'Plan Premium', price: 79.99, features: ['Acceso 24/7 al gimnasio', 'Clases grupales ilimitadas', 'Entrenador personal dedicado'], image: 'https://tengoagujetas.com/wp-content/uploads/2019/04/peso-muerto-fuerza.jpg' },
+  ];
   return (
     <>
-      <Box display="flex" justifyContent="flex-end" alignItems="flex-start" mt={2} mr={2}>
-        <Button endIcon={<CloseIcon />} variant="contained" style={{ backgroundColor: '#FF1744', color: '#FFFFFF' }} href='./menu'></Button>
-      </Box>
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="80vh">
-        <Box width={tableWidth}>
-          <Paper elevation={3} sx={{ background: '#F0F4F5' }}>
-            <TableContainer sx={{ maxHeight: tableHeight }}>
-              <Table>
-                <TableHead>
-                  <TableRow sx={{ background: '#3AC203' }}>
-                    <TableCell>ID</TableCell>
-                    <TableCell>Nombre</TableCell>
-                    <TableCell>Planes</TableCell>
-                    <TableCell>Acciones</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((row) => (
-                    <TableRow key={row.id}>
-                      <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.name}</TableCell>
-                      <TableCell>{row.tipo}</TableCell>
-                      <TableCell>
-                        <IconButton aria-label="editar">
-                          <EditIcon style={{ color: '#1976D2' }} />
-                        </IconButton>
-                        <IconButton aria-label="eliminar">
-                          <DeleteIcon style={{ color: '#D32F2F' }} />
-                        </IconButton>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
-        </Box>
-      </Box>
-    </>
-  );
+            <Box display="flex" justifyContent="flex-end" alignItems="flex-start" mt={2} mr={2}>
+                <Button endIcon={<CloseIcon />} variant="contained" style={{ backgroundColor: '#FF1744', color: '#FFFFFF' }} href='./menu'></Button>
+            </Box>
+            <Box style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
+                {gymPlans.map((plan, index) => (
+                    <Card key={index} sx={{ maxWidth: 280, marginRight: '20px', textAlign: 'center', fontSize:'11px' }}>
+                        <CardMedia
+                            component="img"
+                            height="280"
+                            src={plan.image} 
+                            alt={plan.name}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {plan.name}
+                            </Typography>
+                            <Typography variant="h6" color="textSecondary">
+                                ${plan.price} / mes
+                            </Typography>
+                            <ul style={{ textAlign: 'left' }}>
+                                {plan.features.map((feature, index) => (
+                                    <li key={index}>{feature}</li>
+                                ))}
+                            </ul>
+                        </CardContent>
+                        <Box display="flex" justifyContent="center" marginBottom={1}>
+                            <Button variant="contained" color="primary">
+                                Seleccionar
+                            </Button>
+                        </Box>
+                    </Card>
+                ))}
+            </Box>          
+        </>   
+    );
 }
